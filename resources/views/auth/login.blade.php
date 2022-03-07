02202da4dfc1e3f0498b51a54e04db0c38859da7
Tanzilal Mustaqim <derekln14@gmail.com>
2022-03-07 15:07:16 +0700

    fix logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,11 +5,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('template')}}/assets/images/favicon.png">
+    <link rel="icon" type="image/png" sizes="16x16"
+        href="{{ asset('template') }}/assets/images/fav-klinik.png">
+    >
     <title>Halaman Login</title>
-    <link href="{{asset('template')}}/dist/css/style.css" rel="stylesheet">
+    <link href="{{ asset('template') }}/dist/css/style.css" rel="stylesheet">
     <!-- This page CSS -->
-    <link href="{{asset('template')}}/dist/css/pages/authentication.css" rel="stylesheet">
+    <link href="{{ asset('template') }}/dist/css/pages/authentication.css" rel="stylesheet">
     <!-- This page CSS -->
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -36,12 +38,17 @@
         <!-- ============================================================== -->
         <!-- Login box.scss -->
         <!-- ============================================================== -->
-        <div class="auth-wrapper d-flex no-block justify-content-center align-items-center" style="background:url({{asset('template')}}/assets/images/big/bg_auth.jpg) no-repeat center center; background-size: cover; background-color: #2c3e50;   background-blend-mode: lighten;">
+        <div class="auth-wrapper d-flex no-block justify-content-center align-items-center"
+            style="background:url({{ asset('template') }}/assets/images/big/bg_auth.jpg) no-repeat center center; background-size: cover; background-color: #2c3e50;   background-blend-mode: lighten;">
             <div class="auth-box">
                 <div id="loginform">
                     <div class="logo">
-                        <span class="db"><img src="{{asset('template')}}/assets/images/logo-icon.png" alt="logo" /></span>
-                        <h5 class="font-medium m-b-20">Sign In to Admin</h5>
+                        <span class="db"><img
+                                {{-- src="{{ asset('template') }}/assets/images/logo-icon.png" --}}
+                                src="{{ asset('template') }}/assets/images/fav-klinik.png">
+
+                            alt="logo" /></span>
+                        <h5 class="font-medium m-b-20">Masuk ke Sistem Klinik</h5>
                     </div>
                     <!-- Form -->
                     <div class="row">
@@ -50,34 +57,40 @@
                             <!-- email -->
                             <div class="row">
                                 <div class="input-field col s12">
-                                    <input  id="email" type="email" class="validate form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    <input id="email" type="email"
+                                        class="validate form-control @error('email') is-invalid @enderror" name="email"
+                                        value="{{ old('email') }}" required autocomplete="email"
+                                        autofocus>
                                     <label for="email">Email</label>
                                     @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
                                 </div>
 
                             </div>
                             <!-- pwd -->
                             <div class="row">
                                 <div class="input-field col s12">
-                                    <input id="password" type="password" class="validate form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                    <input id="password" type="password"
+                                        class="validate form-control @error('password') is-invalid @enderror"
+                                        name="password" required autocomplete="current-password">
                                     <label for="password">Password</label>
 
                                     @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
                                 </div>
                             </div>
                             <!-- pwd -->
                             <div class="row m-t-5">
                                 <div class="col s7">
                                     <label>
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }} />
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember"
+                                            {{ old('remember') ? 'checked' : '' }} />
                                         <span>Remember Me?</span>
                                     </label>
                                 </div>
@@ -94,47 +107,48 @@
 
                     {{-- <div class="center-align m-t-20 db">
                         Tidak punya akun? <a href="{{ route('register') }}">Sign Up!</a>
-                    </div> --}}
-                </div>
-
+                </div> --}}
             </div>
+
         </div>
-        <!-- ============================================================== -->
-        <!-- Login box.scss -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Page wrapper scss in scafholding.scss -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Page wrapper scss in scafholding.scss -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Right Sidebar -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Right Sidebar -->
-        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- Login box.scss -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- Page wrapper scss in scafholding.scss -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- Page wrapper scss in scafholding.scss -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- Right Sidebar -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- Right Sidebar -->
+    <!-- ============================================================== -->
     </div>
     <!-- ============================================================== -->
     <!-- All Required js -->
     <!-- ============================================================== -->
-    <script src="{{asset('template')}}/assets/libs/jquery/dist/jquery.min.js"></script>
-    <script src="{{asset('template')}}/dist/js/materialize.min.js"></script>
+    <script src="{{ asset('template') }}/assets/libs/jquery/dist/jquery.min.js"></script>
+    <script src="{{ asset('template') }}/dist/js/materialize.min.js"></script>
     <!-- ============================================================== -->
     <!-- This page plugin js -->
     <!-- ============================================================== -->
     <script>
-    $('.tooltipped').tooltip();
-    // ==============================================================
-    // Login and Recover Password
-    // ==============================================================
-    $('#to-recover').on("click", function() {
-        $("#loginform").slideUp();
-        $("#recoverform").fadeIn();
-    });
-    $(function() {
-        $(".preloader").fadeOut();
-    });
+        $('.tooltipped').tooltip();
+        // ==============================================================
+        // Login and Recover Password
+        // ==============================================================
+        $('#to-recover').on("click", function () {
+            $("#loginform").slideUp();
+            $("#recoverform").fadeIn();
+        });
+        $(function () {
+            $(".preloader").fadeOut();
+        });
+
     </script>
 </body>
 
